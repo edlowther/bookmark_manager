cf8fd52a84f8293cd888430ef888c511dc73b54f
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-17 16:24:57 +0100

    Tweaked implementation of database cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -26,10 +26,12 @@ RSpec.configure do |config|
     DatabaseCleaner.clean_with(:truncation)
   end
 
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
+  config.before(:each) do
+    DatabaseCleaner.start
   end
-  
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
 end
