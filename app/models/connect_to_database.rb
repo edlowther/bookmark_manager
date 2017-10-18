cd64be7368390338a7d37021f48249f3a9f53849
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-18 10:49:03 +0100

    Tweaked postgres config to work with heroku

--- app/models/connect_to_database.rb
+++ app/models/connect_to_database.rb
@@ -3,7 +3,7 @@ require 'dm-postgres-adapter'
 
 module ConnectToDatabase
   def connect_to_database
-    DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
+    DataMapper.setup(:default,  ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
     DataMapper.finalize
     DataMapper.auto_upgrade!
   end
