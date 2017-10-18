bc54f3ea30860bf0f15202b3e5bbab17bf8e6861
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-18 16:02:02 +0100

    typo

--- heroku_helpers/clean_database.rb
+++ heroku_helpers/clean_database.rb
@@ -1,8 +1,8 @@
 require 'dm-migrations'
 require 'data_mapper'
 require 'dm-postgres-adapter'
-require_relative 'models/link'
-require_relative 'models/tag'
+require_relative '../app/models/link'
+require_relative '../app/models/tag'
 DataMapper.setup(:default,  ENV['DATABASE_URL'])
 Link.destroy
 Tag.destroy
