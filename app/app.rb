ca8ae4f37c204279fc42ee95a575cf047e603de4
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-18 10:18:26 +0100

    Tweaked development env configuration

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,10 @@
+ENV["RACK_ENV"] ||= "development"
+
 require 'sinatra/base'
 require_relative 'models/link'
 require_relative 'models/connect_to_database'
 
 class BookmarkManager  < Sinatra::Base
-  ENV["RACK_ENV"] ||= "development"
   get '/links' do
     @links = Link.all
     erb :"links/index"
