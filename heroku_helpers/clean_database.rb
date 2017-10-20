b274083bdefbf20f8463f7313bb00e2e74715928
Ed Lowther <ed.lowther[at]gmail.com>
2017-10-20 16:04:32 +0100

    Added Person class to heroku helpers file

--- heroku_helpers/clean_database.rb
+++ heroku_helpers/clean_database.rb
@@ -3,6 +3,8 @@ require 'data_mapper'
 require 'dm-postgres-adapter'
 require_relative '../app/models/link'
 require_relative '../app/models/tag'
+require_relative '../app/models/person'
 DataMapper.setup(:default,  ENV['DATABASE_URL'])
 Link.destroy
 Tag.destroy
+Person.destroy
